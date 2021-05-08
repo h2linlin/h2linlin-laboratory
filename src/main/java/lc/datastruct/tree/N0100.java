c1c1c04c8f4416bf012f8ba0aede6c2836844f14
h2linlin <wzh443@126.com>
2021-05-08 20:39:52 +0800

    update

---
+++ src/main/java/lc/datastruct/tree/N0100.java
@@ -0,0 +1,31 @@
+package lc.datastruct.tree;
+
+/**
+ * @Desc 100. 相同的树
+ * @Author h2linlin
+ */
+public class N0100 {
+    /**
+     * DFS，递归法
+     */
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return compare(p, q);
+    }
+
+    private boolean compare(TreeNode a, TreeNode b) {
+        if (a == null && b == null) {
+            return true;
+        }
+        if (a != null && b == null) {
+            return false;
+        }
+        if (a == null && b != null) {
+            return false;
+        }
+
+        boolean left = compare(a.left, b.left);
+        boolean right = compare(a.right, b.right);
+
+        return left && right && (a.val == b.val);
+    }
+}
