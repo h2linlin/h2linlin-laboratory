6b1ce09c7c364703ca5a8855857e1914056f3bb2
h2linlin <wzh443@126.com>
2021-07-30 17:51:15 +0800

    update

--- src/main/java/lc/datastruct/dp/N0000.java
+++ src/main/java/lc/datastruct/dp/N0000.java
@@ -1,5 +1,7 @@
 package lc.datastruct.dp;
 
+import lc.DisplayUtil;
+
 /**
  * @Desc 背包问题示例
  * @Author h2linlin
@@ -21,5 +23,49 @@ public class N0000 {
      *      2.不装：不需要给i留空间，也不需要增加i的价值。dp[i][j] = dp[i-1][j]
      *      3.以上两种取最大值即可。即 dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight[i]] + value[i])
      */
+    private static void test2WeiBagProblem1() {
+        // 物品重量
+        int[] weight = {1, 3, 4};
+        // 物品价值
+        int[] value = {15, 20, 30};
+        // 背包容量
+        int bagWeight = 4;
+
+        // 1.dp二维数组
+        int[][] dp = new int[weight.length][bagWeight + 1];
+
+        // 2.初始化
+        // 2.1 背包容量为0时，总价值全部为0。无需初始化。
+        // 2.2 只提供0号物品选择时。能装下时，总价值就是物品0的价值。不能装下时，总价值就是0。
+        // 注意到初始化顺序是倒序，因为不能由前面的结果影响到后面的结果。
+        for (int j = bagWeight; j >= weight[0]; j--) {
+            dp[0][j] = dp[0][j - weight[0]] + value[0];
+        }
+
+        // 3.遍历。先容量方向，后物品方向
+        for (int i = 1; i < weight.length; i++) {   // 物品
+            for (int j = 1; j <= bagWeight; j++) {  // 背包容量
+                if (j < weight[i]) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weight[i]] + value[i]);
+                }
+            }
+        }
+
+        // 4.输出结果
+        DisplayUtil.display(dp);
+    }
+
+    /**
+     * 滚动数组方式，是对上述二维数组方式的简化
+     */
+    private static void test1WeiBagProblem1() {
+
+    }
+
+    public static void main(String[] args) {
+        test2WeiBagProblem1();
+    }
 
 }
